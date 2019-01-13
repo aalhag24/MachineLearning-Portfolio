1b03b782debd2b35d9bd817de6cf824110ec83f1
aalhag24 <aalhag24@gmail.com>
2019-01-13 12:49:45 -0800

    Finished Python SLR model

--- 2-Regression/1_Simple_Linear_Regression/src/simple_linear_regression.py
+++ 2-Regression/1_Simple_Linear_Regression/src/simple_linear_regression.py
@@ -8,6 +8,7 @@ import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 # Import the data
 Path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'bin\Salary_Data.csv'))
@@ -19,4 +20,26 @@ Y = dataset.iloc[:, 1].values
 # Splitting the dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
+# Fit Simple Linear Regression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predict the Test set
+Y_pred = regressor.predict(X_test)
+
+# Visualising the Training set
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Testing set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
