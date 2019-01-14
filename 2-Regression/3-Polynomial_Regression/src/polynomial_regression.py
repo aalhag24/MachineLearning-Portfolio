1a3d295078f15cdcd497c3a1dd60ead61f941e9f
aalhag24 <aalhag24@gmail.com>
2019-01-14 11:27:50 -0800

    Visualizing the Polynomial Results in degree 2, 3, 4

--- 2-Regression/3-Polynomial_Regression/src/polynomial_regression.py
+++ 2-Regression/3-Polynomial_Regression/src/polynomial_regression.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # Import the data
-Path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'bin\\Position_Salary.csv'))
+Path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'bin\\Position_Salaries.csv'))
 dataset = pd.read_csv(Path)
 
 X = dataset.iloc[:, 1:2].values
@@ -27,8 +27,54 @@ lin_reg = LinearRegression()
 lin_reg.fit(X, Y);
 
 # Fitting Polnomial Regressoin to the data
-poly_reg = PolynomialFeatures(degree=2)
-X_poly = poly_reg.fit_transform(X)
+poly_reg_deg_2 = PolynomialFeatures(degree=2)
+poly_reg_deg_3 = PolynomialFeatures(degree=3)
+poly_reg_deg_4 = PolynomialFeatures(degree=4)
 
-lin_reg2 = LinearRegression()
-lin_reg2.fit(X_poly, Y)
+X_poly_deg_2 = poly_reg_deg_2.fit_transform(X)
+X_poly_deg_3 = poly_reg_deg_3.fit_transform(X)
+X_poly_deg_4 = poly_reg_deg_4.fit_transform(X)
+
+lin_reg_deg_2 = LinearRegression()
+lin_reg_deg_3 = LinearRegression()
+lin_reg_deg_4 = LinearRegression()
+
+lin_reg_deg_2.fit(X_poly_deg_2, Y)
+lin_reg_deg_3.fit(X_poly_deg_3, Y)
+lin_reg_deg_4.fit(X_poly_deg_4, Y)
+
+# Visualising the Linear Regression results
+LinearPlot = plt.figure(1);
+plt.scatter(X, Y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff Salary Tester (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+LinearPlot.show()
+
+# Visualising the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+PolyPlot_deg_2 = plt.figure(2);
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, lin_reg_deg_2.predict(poly_reg_deg_2.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff Salary Tester (Degree 2 Poly)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+PolyPlot_deg_2.show()
+
+PolyPlot_deg_3 = plt.figure(3);
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, lin_reg_deg_3.predict(poly_reg_deg_3.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff Salary Tester (Degree 3 Poly)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+PolyPlot_deg_3.show()
+
+PolyPlot_deg_4 = plt.figure(4);
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, lin_reg_deg_4.predict(poly_reg_deg_4.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff Salary Tester (Degree 4 Poly)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
