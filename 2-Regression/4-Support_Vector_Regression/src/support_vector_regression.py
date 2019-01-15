71a0e6c70b8623e4296a5d8c7823ff9e87e43bd5
aalhag24 <aalhag24@gmail.com>
2019-01-14 17:39:22 -0800

    Debugged and Finished Python SVR

--- 2-Regression/4-Support_Vector_Regression/src/support_vector_regression.py
+++ 2-Regression/4-Support_Vector_Regression/src/support_vector_regression.py
@@ -4,14 +4,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import statsmodels.formula.api as sm
 import os
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.svm import SVR
 
 # Import the data
 Path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'bin\\Position_Salaries.csv'))
 dataset = pd.read_csv(Path)
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2:].values
+
+# Feature scaling
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+Y = sc_Y.fit_transform(Y)
+#Y = pd.DataFrame(Y)
+
+# Fitting a new result
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X,Y)
+
+# Predicting a new result
+Y_pred_ = sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+# Visualising the SVR result
+SVR_Plot = plt.figure(1)
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth of Bluff (SVR)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution)
+X_grid = np.arrange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff Salary Tester (SVR Model)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
